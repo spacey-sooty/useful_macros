7a445f9d40414851ef0694478ed7b442036a93a9
Isaac Turner <spacey_sooty@outlook.com>
2023-10-27 10:00:30 +0800

    larger unit test

--- proc_macros/tests/from.rs
+++ proc_macros/tests/from.rs
@@ -3,7 +3,7 @@ mod test {
     use proc_macros::From;
 
     #[test]
-    fn simple_expansion() {
+    fn small_expansion() {
         #[derive(From, Debug, PartialEq)]
         struct Foo {
             name: String,
@@ -13,6 +13,38 @@ mod test {
             Foo::from(String::from("Hello")),
             Foo {
                 name: String::from("Hello")
+            }
+        );
+    }
+
+    #[test]
+    fn large_expansion() {
+        #[derive(PartialEq, Debug)]
+        enum Bar {
+            Foo(String),
+            Bar(bool),
+        }
+        #[derive(From, Debug, PartialEq)]
+        struct Foo {
+            name: String,
+            foo: i32,
+            bar: Bar,
+        }
+
+        assert_eq!(
+            Foo::from(String::from("Hello"), 7, Bar::Bar(true)),
+            Foo {
+                name: String::from("Hello"),
+                foo: 7,
+                bar: Bar::Bar(true)
+            }
+        );
+        assert_eq!(
+            Foo::from(String::from("Hello"), 7, Bar::Foo(String::from("FooBar"))),
+            Foo {
+                name: String::from("Hello"),
+                foo: 7,
+                bar: Bar::Foo(String::from("FooBar"))
             }
         );
     }
